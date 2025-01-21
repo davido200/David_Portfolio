297002c5e3f93baeea01846e2fbd7a9ab74790d1
David Kemboi <Dkemboi888@gmail.com>
2025-01-21 14:59:11 +0300

    Update_2

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -103,7 +103,7 @@ function Footer() {
           <SocialMediaIcon href={Bio.insta} target="display"><InstagramIcon /></SocialMediaIcon>
         </SocialMediaIcons>
         <Copyright>
-          &copy; 2024 Kemboi K David. All rights reserved.
+          &copy; 2025 Kemboi K David. All rights reserved.
         </Copyright>
 
       </FooterWrapper>
